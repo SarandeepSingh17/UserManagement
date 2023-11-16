609641bf7a915f041d4976cdf2f1513ca476fa50
Sarandeep Singh <70137752+SarandeepSingh17@users.noreply.github.com>
2023-11-16 19:38:16 +0530

    Update App.js
    Installed React Router and Routes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AccountCreation from './components/AccountCreation';
 import UserDetails from './components/UserDetails';
 import './App.css';
-
 
 function App() {
   const [accountData, setAccountData] = useState(null);
@@ -10,16 +10,28 @@ function App() {
   const handleAccountCreated = (data) => {
     setAccountData(data);
   };
-  
 
   return (
-    
-    <div>
-      <UserDetails accountData={accountData} />
-      <AccountCreation onAccountCreated={handleAccountCreated}/>
-      </div>
-    
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/user-details">User Details</Link>
+            </li>
+            <li>
+              <Link to="/account-creation">Account Creation</Link>
+            </li>
+          </ul>
+        </nav>
 
-  }
+        <Routes>
+          <Route path="/user-details" element={<UserDetails accountData={accountData} />} />
+          <Route path="/account-creation" element={<AccountCreation onAccountCreated={handleAccountCreated} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
 export default App;
